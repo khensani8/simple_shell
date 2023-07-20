df676beeb8ea69141c43dd0e66498266b1d647eb
Khensani Ngobeni <khensi.ngobeni@gmail.com>
2023-07-20 05:17:18 -0700

    betty fixes

--- shell.c
+++ shell.c
@@ -1,27 +1,18 @@
 #include "shell.h"
 
-/*
- * rd_line - read single line from input
+/**
+ *rd_line - read single line from input
  *
- * Return: command input
+ *Return: command input
  */
 char *rd_line(void)
 {
-    char *line = NULL;
-    size_t n = 0;
+	char *line = NULL;
+	size_t n = 0;
 
-    if (isatty(STDIN_FILENO))
-        write(STDOUT_FILENO, "($) ", 4);
+	getline(&line, &n, stdin);
 
-    if (getline(&line, &n, stdin) == -1)
-    {
-        if (isatty(STDIN_FILENO))
-            write(STDOUT_FILENO, "\n", 1);
-        free(line);
-        return NULL;
-    }
-
-    return line;
+	return (line);
 }
 
 /**
@@ -33,30 +24,30 @@ char *rd_line(void)
  */
 char **split_line(char *line)
 {
-    int i, bufsize;
-    char *delim, *tok;
-    char **toks;
+	int i, bufsize;
+	char *delim, *tok;
+	char **toks;
 
-    bufsize = 64;
-    delim = " \t\n\r\a";
-    toks = malloc(bufsize * sizeof(char *));
+	bufsize = 64;
+	delim = " \t\n\r\a";
+	toks = malloc(bufsize * sizeof(char *));
+	i = 0;
+	tok = strtok(line, delim);
 
-    i = 0;
-    tok = strtok(line, delim);
-    while (tok != NULL)
-    {
-        toks[i] = tok;
-        i++;
-        if (i >= bufsize)
-        {
-            bufsize += 64;
-            toks = realloc(toks, bufsize * sizeof(char *));
-        }
+	while (tok != NULL)
+	{
+		toks[i] = tok;
+		i++;
 
-        tok = strtok(NULL, delim);
-    }
-    toks[i] = NULL;
-    return toks;
+		if (i >= bufsize)
+		{
+			bufsize += 64;
+			toks = realloc(toks, bufsize * sizeof(char *));
+		}
+		tok = strtok(NULL, delim);
+	}
+	toks[i] = NULL;
+	return (toks);
 }
 
 /**
@@ -68,68 +59,72 @@ char **split_line(char *line)
  */
 int act(char **ar)
 {
-    pid_t child = fork();
+	pid_t child = fork();
 
-    if (child == 0)
-    {
-        if (execve(ar[0], ar, NULL) == -1)
-        {
-            perror("hsh");
-            exit(EXIT_FAILURE);
-        }
-    }
-    else if (child < 0)
-    {
-        perror("hsh");
-    }
-    else
-    {
-        int status;
-        wait(&status); /* Wait for the child process to complete */
-    }
+	if (child == 0)
+	{
+		if (execve(ar[0], ar, NULL) == -1)
+		{
+			perror("hsh");
+			exit(EXIT_FAILURE);
+		}
+	}
+	else if (child < 0)
+	{
+		perror("hsh");
+	}
+	else
+	{
+		int status;
 
-    return 1;
+		wait(&status); /* Wait for the child process to complete */
+	}
+	return (1);
 }
 
 /**
  * main - initializes shell and runs
  *
+ * @argc: number of command-line arguments
+ * @argv: array of command-line arguments
+ *
  * Return: Always 0
  */
-int main(void)
+int main(int argc, char *argv[])
 {
-    char *line, **tok;
+	if (argc == 1)
+	{
+		char *line, **tok;
 
-    while (1)
-    {
-        line = rd_line();
-        if (line == NULL)
-        {
-            break;
-        }
+		while (1)
+		{
+			/* prompt */
+			write(STDOUT_FILENO, "($) ", 4);
+			line = rd_line();
+			tok = split_line(line);
 
-        tok = split_line(line);
-        if (tok[0] != NULL)
-        {
-            if (strcmp(tok[0], "exit") == 0)
-            {
-                /* Free allocated memory before exiting */
-                free(tok);
-                free(line);
-                if (isatty(STDIN_FILENO))
-		
-			break;
-            }
-            else
-            {
-                act(tok);
-            }
-        }
-
-        free(tok);
-        free(line);
-    }
-
-    return 0;
+			if (tok[0] != NULL)
+			{
+				if (strcmp(tok[0], "exit") == 0)
+				{
+					/* Free allocated memory before exiting */
+					free(tok);
+					free(line);
+					break; /* Exit the while loop */
+				}
+				else
+				{
+					act(tok);
+				}
+			}
+			free(tok);
+			free(line);
+		}
+	}
+	else
+	{
+		/* Command-line mode */
+		act(&argv[1]);
+	}
+	return (0);
 }
-
